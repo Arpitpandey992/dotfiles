ffdb08bd910c99ba7fef4f74d64c0aff163bae95
Arpitpandey992 <arpitpandey992@gmail.com>
2024-04-01 12:15:22 +0530

    fix tag function in fish to point to latest changes

--- fish/functions/tag.fish
+++ fish/functions/tag.fish
@@ -1,4 +1,4 @@
 function tag --wraps='python ~/Programming/Python/VGMDB-Auto-Tagger/albumTagger.py' --description 'alias tag=python ~/Programming/Python/VGMDB-Auto-Tagger/albumTagger.py'
   source /home/arpit/Programming/Python/VGMDB-Auto-Tagger/.venv/bin/activate.fish
-  python ~/Programming/Python/VGMDB-Auto-Tagger/albumTagger.py $argv;
+  python ~/Programming/Python/VGMDB-Auto-Tagger/album_tagger.py $argv;
 end
